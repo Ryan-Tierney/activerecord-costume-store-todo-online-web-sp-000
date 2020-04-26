10bb0e1112948e5d1b55e4cd421d52cdfac50f50
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-26 16:05:43 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,13 @@
-# Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[5.2] 
+  def change 
+  create_table :haunted_houses do |t| 
+    t.string :name 
+    t.string :location 
+    t.float :price 
+    t.boolean :whether_theyre_family_friendly_or_not 
+    t.datetime :opening_date 
+    t.datetime :closing_date 
+    t.string :long_description 
+    end 
+  end 
+end 
