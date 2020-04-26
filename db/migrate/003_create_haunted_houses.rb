a352ba272a2a0a9d5a2e4603ba484f98f29930d4
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-26 16:11:10 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -8,7 +8,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
     t.boolean :whether_theyre_family_friendly_or_not 
     t.datetime :opening_date 
     t.datetime :closing_date 
-    t.string :long_description 
+    t.text :long_description 
     end 
   end 
 end 
